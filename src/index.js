8c2ded450c5cb8428876082182300e148c725c9e
Aaron Osher <aaron@oshers.com>
2018-08-19 13:58:50 +0100

    Update index
    Add store provider
    Update index
    
    fix Provider import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.getElementById('root'),
+  );
+};
+
+render(App);
 registerServiceWorker();
