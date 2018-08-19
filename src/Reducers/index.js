0d80ff17ff4a376fd33019d05a1ef8e4f13b8b40
Aaron Osher <aaron@oshers.com>
2018-08-19 13:46:48 +0100

    Update Reducers index
    combined firebase and firestore reducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as forms } from 'redux-forms';
+import { firebaseReducer, firestoreReducer } from './firebase';
 
 const coreReducers = combineReducers({
   forms,
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
 });
 
 export default coreReducers;
