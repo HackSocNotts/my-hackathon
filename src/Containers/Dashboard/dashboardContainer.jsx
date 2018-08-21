20e662d9b5e14ea0414b29a831832a402a0a4f36
Aaron Osher <aaron@oshers.com>
2018-08-21 10:22:14 +0100

    Add Dashboard Container

---
+++ src/Containers/Dashboard/dashboardContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './styles';
+
+class dashboardContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default dashboardContainer;
