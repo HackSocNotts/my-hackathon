88648acef3c50689271ad70d516769b2a6dd8a7d
Aaron Osher <aaron@oshers.com>
2018-08-23 00:47:11 +0100

    Update App
    Implement Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import history from './Store/history';
 import LoginPage from './Pages/Login';
 import HomePage from './Pages/Home';
+import theme from './theme';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router history={history}>
-        <Switch>
-          <Route path="/" component={HomePage} exact />
-          <Route path="/login" component={LoginPage} />
-        </Switch>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router history={history}>
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/login" component={LoginPage} />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
