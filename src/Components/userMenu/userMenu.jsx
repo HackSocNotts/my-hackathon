27d52516d441c02a1006abf2e7da18255db91e38
Aaron Osher <aaron@oshers.com>
2018-08-23 00:54:29 +0100

    Add userMenu component

---
+++ src/Components/userMenu/userMenu.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import styles from './styles';
+
+class userMenu extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(userMenu);
