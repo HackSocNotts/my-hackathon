38e98a80433a644e57dada2e0c8148b3a5320a07
Aaron Osher <aaron@oshers.com>
2018-08-22 19:52:01 +0100

    Add Home Page

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DashboardContainer from '../../Containers/Dashboard';
+
+class home extends Component {
+  render() {
+    return (
+      <DashboardContainer>
+        Hello World
+      </DashboardContainer>
+    );
+  }
+}
+
+export default home;
