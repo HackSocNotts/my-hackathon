bdea7dc36aa845576b17ddd9b905cc157314e5fd
Aaron Osher <aaron@oshers.com>
2018-08-23 01:28:31 +0100

    Update dashboard container
    Use notificationsMenu component

--- src/Containers/Dashboard/dashboardContainer.jsx
+++ src/Containers/Dashboard/dashboardContainer.jsx
@@ -8,10 +8,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import NotificationsMenu from '../../Components/notificationsMenu';
 import styles from './styles';
 
 class dashboardContainer extends Component {
@@ -69,11 +68,7 @@ class dashboardContainer extends Component {
               <Typography variant="title" color="inherit" noWrap className={classes.title}>
                 Dashboard
               </Typography>
-              <IconButton color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
+              <NotificationsMenu />
             </Toolbar>
           </AppBar>
           <Drawer
