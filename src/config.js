bbd831a916d2c829c1b7e86fa65e4d81c780b06d
Aaron Osher <aaron@oshers.com>
2018-08-23 03:12:35 +0100

    Update config.js
    Add comments
    Add presence

--- src/config.js
+++ src/config.js
@@ -1,3 +1,13 @@
+/**
+ * Some variable are environment based and must be set in the .env file
+ * @file configuration file for myHackathon.
+ * @author Aaron Osher
+ */
+
+/**
+ * Firebase configuartion. Values can be retrieved from the firebase console
+ * Set in the .env file
+ */
 const firebase = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -7,12 +17,42 @@ const firebase = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
+/**
+ * react-redux-firebase configurateion
+ */
 const reduxFirebase = {
+  /**
+   * Presence keeps a list in firestore of which users are currently online
+   * @default undefined
+   */
+  // presence: 'presence',
+  /**
+   * sessions keeps a history of presence
+   * presence must set to true
+   * @default undefined
+   */
+  // sessions: 'sessions',
+  /**
+   * User profile location in firestore
+   * Do not change this value
+   */
   userProfile: 'users',
+  /**
+   * Firestore must be enabled in the console
+   * Do not change this value
+   */
   useFirestoreForProfile: true,
 };
 
+/**
+ * configuration varaibles for myHackathon
+ */
 const siteVars = {
+  /**
+   * Name of the hackathon/myHackathon portal e.g. myHackNotts
+   * Set in .env file
+   * @default myHackathon
+   */
   hackathonName: process.env.REACT_APP_NAME,
 };
 
