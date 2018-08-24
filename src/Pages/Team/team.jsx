b4ec705045d3db3b7dd96deff2d4d1a8c41fb8f0
Aaron Osher <aaron@oshers.com>
2018-08-24 01:45:53 +0100

    Add Team Page

---
+++ src/Pages/Team/team.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import DashboardContainer from '../../Containers/Dashboard';
+
+export default class team extends Component {
+  render() {
+    return (
+      <DashboardContainer>
+        Team Works
+      </DashboardContainer>
+    );
+  }
+}
