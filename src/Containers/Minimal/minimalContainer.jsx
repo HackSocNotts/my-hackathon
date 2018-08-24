8bef5f740e74655ed5c1981ac50495bb19c2c1e4
Aaron Osher <aaron@oshers.com>
2018-08-24 22:48:21 +0100

    Update Minimal Container
    Add Flash

--- src/Containers/Minimal/minimalContainer.jsx
+++ src/Containers/Minimal/minimalContainer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import withStyles from '@material-ui/core/styles/withStyles';
+import Flash from '../../Components/Flash';
 import styles from './styles';
 
 class minimalContainer extends Component {
@@ -13,6 +14,7 @@ class minimalContainer extends Component {
         <main className={classes.layout}>
           <Paper className={classes.paper}>
             {children}
+            <Flash cardProps={{ elevation: 0, style: { marginTop: 24 } }} />
           </Paper>
         </main>
       </React.Fragment>
