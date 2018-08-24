72f1c23e855a97b9aa8bcb25aec26703c8486ca3
Aaron Osher <aaron@oshers.com>
2018-08-24 23:18:46 +0100

    Update dashboard drawer
    only show expanded on desktop

--- src/Modules/Navigation/initialState.js
+++ src/Modules/Navigation/initialState.js
@@ -1,7 +1,13 @@
+import { isMobile } from 'react-device-detect';
+
 const navigationInitialState = {
   drawer: {
     open: true,
   },
 };
+
+if (isMobile) {
+  navigationInitialState.drawer.open = false;
+}
 
 export default navigationInitialState;
