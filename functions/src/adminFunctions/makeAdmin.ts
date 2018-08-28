bfb3de12c4afe0f681156590dea552254bcefd27
Aaron Osher <aaron@oshers.com>
2018-08-28 10:54:37 +0100

    Update makeAdmin
    Updates firestore profiel

--- functions/src/adminFunctions/makeAdmin.ts
+++ functions/src/adminFunctions/makeAdmin.ts
@@ -1,11 +1,18 @@
-import { auth } from 'firebase-admin';
+import { auth, firestore } from 'firebase-admin';
 import { assignClaim } from '../utils';
 
 const makeAdmin = async (data, context) => {
   const issuer = await auth().getUser(context.auth.uid);
   const target = await auth().getUser(data.uid);
 
-  return assignClaim(issuer, target, { admin: true });
+  return firestore().doc(`users/${target.uid}`).update({
+    admin: true,
+  })
+    .then(() => assignClaim(issuer, target, { admin: true }))
+    .catch(err => {
+      console.error(err);
+      return { error: 'Unkown server error' };
+    });
 };
 
 export default makeAdmin;
