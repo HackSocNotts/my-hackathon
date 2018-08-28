b80187d4f6fcc0b8f6a54d8de8a5ef1d34526a7e
Aaron Osher <aaron@oshers.com>
2018-08-28 10:55:20 +0100

    Update assignClaim
    Uses onCall response format

--- functions/src/utils/assignClaim.ts
+++ functions/src/utils/assignClaim.ts
@@ -4,18 +4,18 @@ import logMessage, { LogType } from './log';
 const assignClaim = (issuer: auth.UserRecord, target: auth.UserRecord, claim: any) => {
   if (!issuer.customClaims || !issuer.customClaims['admin']) {
     return logMessage(issuer, target, `Attempted to assign: ${JSON.stringify(claim)}`, LogType.ClaimAssignment)
-      .then(() => ({ success: false, error: 'Issuer not an admin' }))
+      .then(() => ({ error: 'Issuer not an admin' }))
       .catch(err => {
         console.error(err);
-        return { success: false, error: 'Issuer not an admin' }
+        return { error: 'Issuer not an admin' }
       });
   }
   return auth().setCustomUserClaims(target.uid, claim)
     .then(() => logMessage(issuer, target, `Assign: ${JSON.stringify(claim)}`, LogType.ClaimAssignment))
-    .then(() => ({ success: true }))
+    .then(() => ({ data: { success: true } }))
     .catch(err => {
       console.error(err);
-      return { success: false, error: 'Unknown Error' };
+      return {  error: 'Unknown Error' };
     });
 };
 
