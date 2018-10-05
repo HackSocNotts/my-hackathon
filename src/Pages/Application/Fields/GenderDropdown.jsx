3063d56d352be9a9dc5eec840546ce3905629d7a
Aaron Osher <aaron@oshers.com>
2018-10-05 09:45:40 +0100

    Add Application Dropdowns
    Dietary Dropdown
    Ethnicity Dropdown
    Gender Dropdown
    School Search
    Shirt Size

---
+++ src/Pages/Application/Fields/GenderDropdown.jsx
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
+
+import CreatableSelect from 'react-select/lib/Creatable';
+
+/* eslint-disable react/destructuring-assignment */
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 250,
+  },
+  input: {
+    display: 'flex',
+    padding: 0,
+  },
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
+      0.08,
+    ),
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+  },
+  singleValue: {
+    fontSize: 16,
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 2,
+    fontSize: 16,
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
+});
+
+const NoOptionsMessage = props => (
+  <Typography
+    color="textSecondary"
+    className={props.selectProps.classes.noOptionsMessage}
+    {...props.innerProps}
+  >
+    {props.children}
+  </Typography>
+);
+
+const inputComponent = ({ inputRef, ...props }) => <div ref={inputRef} {...props} />;
+
+const Control = props => (
+  <TextField
+    fullWidth
+    InputProps={{
+      inputComponent,
+      inputProps: {
+        className: props.selectProps.classes.input,
+        inputRef: props.innerRef,
+        children: props.children,
+        ...props.innerProps,
+      },
+    }}
+    {...props.selectProps.textFieldProps}
+  />
+);
+
+const Option = props => (
+  <MenuItem
+    buttonRef={props.innerRef}
+    selected={props.isFocused}
+    component="div"
+    style={{
+      fontWeight: props.isSelected ? 500 : 400,
+    }}
+    {...props.innerProps}
+  >
+    {props.children}
+  </MenuItem>
+);
+
+const Placeholder = props => (
+  <Typography
+    color="textSecondary"
+    className={props.selectProps.classes.placeholder}
+    {...props.innerProps}
+  >
+    {props.children}
+  </Typography>
+);
+
+const SingleValue = props => (
+  <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
+    {props.children}
+  </Typography>
+);
+
+const ValueContainer = props => <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
+
+const Menu = props => (
+  <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
+    {props.children}
+  </Paper>
+);
+
+const components = {
+  Control,
+  Menu,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer,
+};
+
+class GenderDropdown extends Component {
+  render() {
+    const { profile, classes, theme } = this.props;
+
+    const selectStyles = {
+      input: base => ({
+        ...base,
+        color: theme.palette.text.primary,
+        '& input': {
+          font: 'inherit',
+        },
+      }),
+    };
+
+    const options = [
+      { label: 'Male', value: 'Male' },
+      { label: 'Female', value: 'Female' },
+      { label: 'Prefer Not to Answer', value: 'Prefer Not to Answer' },
+      { label: 'Other (please specify)', value: 'Other' },
+    ];
+
+    return (
+      <React.Fragment>
+        <CreatableSelect
+          id="school"
+          displayEmpty
+          classes={classes}
+          styles={selectStyles}
+          defaultValue={profile.myMlhData ? { label: profile.myMlhData.gender, value: profile.myMlhData.gender } : ''}
+          options={options}
+          components={components}
+          isClearable
+          textFieldProps={{
+            label: 'Gender',
+            InputLabelProps: {
+              shrink: true,
+            },
+          }}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+GenderDropdown.propTypes = {
+  profile: PropTypes.any.isRequired,
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(GenderDropdown);
