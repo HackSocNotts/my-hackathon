09ed5e9e14d65a5f803a5030ff13223a6ca157f3
Aaron Osher <aaron@oshers.com>
2018-10-05 09:46:07 +0100

    Update application styles
    Add the form margins and flex for application

--- src/Pages/Application/styles.js
+++ src/Pages/Application/styles.js
@@ -1,5 +1,18 @@
 const styles = theme => ({
-
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  margin: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  },
 });
+
+// eslint-disable-next-line
+const mapStateToProps = state => ({ });
+
+// eslint-disable-next-line
+const mapDispatchToProps = dispatch => ({ });
 
 export default styles;
