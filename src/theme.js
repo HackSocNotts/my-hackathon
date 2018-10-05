fab9a122715cc7de452c44ffe365d93fb6bfbb83
Aaron Osher <aaron@oshers.com>
2018-10-05 09:47:07 +0100

    Update Theme File
    I fixed it!!!
    TYPOOS 😭

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,21 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import green from '@material-ui/core/colors/green';
 import blueGrey from '@material-ui/core/colors/blueGrey';
 
 const theme = createMuiTheme({
-  pallete: {
+  palette: {
     secondary: {
-      main: green.A700,
-    },
-    primary: {
       light: blueGrey[300],
       main: blueGrey[500],
       dark: blueGrey[700],
     },
-    type: 'dark',
+    primary: {
+      light: '#77c36b',
+      main: '#56b447',
+      dark: '#3c7d31',
+      contrastText: '#FFF',
+    },
+    type: 'light',
   },
 });
 
