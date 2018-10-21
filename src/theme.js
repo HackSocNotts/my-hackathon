fa48eb92e16bdfa08a72a7a8a3b3e3cc765432b7
Aaron Osher <aaron@oshers.com>
2018-10-21 18:47:24 +0100

    Update theme
    Made Theme based on react website

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,17 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import green from '@material-ui/core/colors/green';
-import blueGrey from '@material-ui/core/colors/blueGrey';
 
 const theme = createMuiTheme({
   palette: {
     secondary: {
-      light: blueGrey[300],
-      main: blueGrey[500],
-      dark: blueGrey[700],
+      light: '#80e1fb',
+      main: '#61DAFB',
+      dark: '#4398af',
     },
     primary: {
-      light: '#77c36b',
-      main: '#56b447',
-      dark: '#3c7d31',
-      contrastText: '#FFF',
+      light: '##41444c',
+      main: '#282c34',
+      dark: '#20232a',
+      contrastText: '#fff',
     },
     type: 'light',
   },
