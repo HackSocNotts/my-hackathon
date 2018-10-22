e59c8fe4c5fea9ec3577a20b3dfc06cf1dda05a9
Aaron Osher <aaron@oshers.com>
2018-10-22 17:44:52 +0100

    Add Eventrbite event retrieval to admin page

--- src/Pages/Admin/admin.jsx
+++ src/Pages/Admin/admin.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+import withStyles from '@material-ui/core/styles/withStyles';
 import DashboardContainer from '../../Containers/Dashboard';
 import { authorizeEventbrite } from '../../Utlis/Eventbrite';
+import EventbriteEventsDropdown from './Fields/EventbriteEventsDropdown';
+import styles from './styles';
 
-export default class admin extends Component {
+class admin extends Component {
   render() {
+    const { events } = this.props;
     return (
       <DashboardContainer>
         <Card>
           <CardContent>
-            <Typography align="left" variant="h3" gutterBottom>
+            <Typography align="left" variant="display2" gutterBottom>
             Eventbrite
             </Typography>
             <Typography variant="body1" gutterBottom>
@@ -20,12 +27,44 @@ export default class admin extends Component {
               to confirm their details here, please authorize eventbrite and
               select your event.
             </Typography>
-            <Button onClick={authorizeEventbrite}>
+            {!!events.length && (
+              <EventbriteEventsDropdown
+                options={events.map(event => ({
+                  label: event.name.text,
+                  value: event.id,
+                }))}
+              />
+            )}
+          </CardContent>
+          <CardActions>
+            {!events.length && (
+            <Button onClick={authorizeEventbrite} size="small">
               Authorize Eventbrite
             </Button>
-          </CardContent>
+            )}
+            {events.length && (
+            <Button onClick={console.log} size="small">
+              Save
+            </Button>
+            )}
+          </CardActions>
         </Card>
       </DashboardContainer>
     );
   }
 }
+
+admin.propTypes = {
+  events: PropTypes.array.isRequired,
+  // classes: PropTypes.object.isRequired,
+};
+
+// eslint-disable-next-line
+const mapStateToProps = state => ({
+  events: state.eventbrite.events,
+});
+
+// eslint-disable-next-line
+const mapDispatchToProps = dispatch => ({ });
+
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(admin));
